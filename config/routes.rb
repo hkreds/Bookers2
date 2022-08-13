0aa98a12c17dba7456b70cc17ad44fdbdf94b19e
hkreds <109258421+hkreds@users.noreply.github.com>
2022-08-13 06:52:58 +0000

    [Add]header・footerレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: 'about'
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
 end
