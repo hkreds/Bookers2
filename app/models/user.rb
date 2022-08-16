23e3771ed80f8d77b256b91a2d201bc5594715ee
hkreds <109258421+hkreds@users.noreply.github.com>
2022-08-16 01:08:16 +0000

    [Add]User更新バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
 
-         has_one_attached :profile_image
+  has_one_attached :profile_image
+
+  validates :name, length: { minimum: 2, maximum: 10 }
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(width, height)
     unless profile_image.attached?
